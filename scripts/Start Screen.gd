661b882a06798301244b87b0b38d95b5dfd1bdbe
Zeno3463 <zenolim3463@gmail.com>
2021-12-03 07:22:07 +0800

    added branding scene

--- scripts/Start Screen.gd
+++ scripts/Start Screen.gd
@@ -1,10 +1,13 @@
 extends Control
 
-export var disclaimer_sign_duration = 5
+export var brand_duration = 3
 
 func _ready():
 	if OS.get_name() == "HTML5":
-		$AnimationPlayer.play("Fade In")
+		$AnimationPlayer.play("Load Disclaimer")
 		yield($AnimationPlayer, "animation_finished")
-		yield(get_tree().create_timer(disclaimer_sign_duration), "timeout")
-		$AnimationPlayer.play_backwards("Fade In")
+	$AnimationPlayer.play("Load Brand")
+	yield(get_tree().create_timer(brand_duration), "timeout")
+	$AnimationPlayer.play_backwards("Load Brand")
+	yield($AnimationPlayer, "animation_finished")
+	$AnimationPlayer.play("Fade Out")
